058fc9ceddbd40e88f4c35f050c8478e7f02e35a
胡力群 <>
2026-03-28 08:16:12 +0800

    add health

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 import admin from './admin'
 import auth from './auth'
-import test from './test'
+import health from './health'
 
 export default [
   { url: '/api/auth', handler: auth },
   { url: '/api/admin', handler: admin },
-  { url: '/api/test', handler: test },
+  { url: '/health', handler: health },
 ]
